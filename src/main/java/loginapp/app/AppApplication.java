376a3c85e9f80973ec143779a4ee1ad035717aff
Geetha1904 <geetha.26csa@licet.ac.in>
2025-04-21 14:30:59 +0530

    First Commit

---
+++ src/main/java/loginapp/app/AppApplication.java
@@ -0,0 +1,14 @@
+package loginapp.app;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppApplication {
+
+	public static final char[] Hello = null;
+
+	public static void main(String[] args) {
+		System.out.println(Hello);
+	}
+
+}
